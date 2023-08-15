d07b9b43c76785ce6fcdca7bc619f7e0464dd7f2
Gabriel-Trajano <slashnebulous02@gmail.com>
2023-08-15 13:37:59 -0300

    Responsividade

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,9 @@
-document.querySelector(".cards .card").addEventListener("click", () => {
+document.querySelector(".cards #course").addEventListener("click", () => {
   window.open("https://go.hotmart.com/I85833861S");
+});
+
+document.querySelector(".cards #ebookJs").addEventListener("click", () => {
+  window.open(
+    "https://drive.google.com/file/d/1mJNsAhFBTMV7093EfixcmABmOkkqgXko/view?usp=drive_link"
+  );
 });
